539bff25fab78d1bd7125a0b71b50b5da63903b9
jasmine alries <alriesjasmine@gmail.com>
2023-09-14 14:56:50 -0400

    title change

---
+++ js/challenge.js
@@ -0,0 +1,30 @@
+{/* <html>
+<head>
+  <meta charset="UTF-8">
+  <title>Dom Challenge</title>
+  <style>
+    #counter {
+      background-color: black;
+      color: white;
+      display: table;
+      padding: 10px;
+    }
+  </style>
+</head> */}
+
+{/* <body> */}
+  {/* <h1>
+    Welcome to the DOM Modification
+  </h1> */}
+
+
+document.querySelector('h1')
+const newTitle = document.querySelector('h1')
+newTitle.innerText(console.log ('Hello, World!'))
+
+
+{/* document.querySelector('counter')
+const counterUp = document.querySelector('counter')
+counterUp.addEventListener('click', function{
+    console.log('hello')
+}) */}
